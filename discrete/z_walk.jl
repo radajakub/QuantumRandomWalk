19ceafed54e589df88706399020657938908c5bd
Jakub Rada <dev.jakubrada@icloud.com>
2024-06-07 12:56:58 +0200

    Move the previous simulation to a single file

--- discrete/z_walk.jl
+++ discrete/z_walk.jl
@@ -1,4 +1,17 @@
-include("state.jl")
+using Plots
+
+struct QState
+    coefficient::Complex
+    coin::Int
+    vertex::Int
+
+    QState(coefficient, coin, vertex) = new(complex(coefficient), coin, vertex)
+end
+
+struct State
+    probability::Real
+    vertex::Int
+end
 
 split(qs::QState, c1::Complex, c2::Complex) = [QState(c1 * qs.coefficient, 0, qs.vertex), QState(c2 * qs.coefficient, 1, qs.vertex)]
 increase(qs::QState) = QState(qs.coefficient, qs.coin, qs.vertex + 1)
@@ -108,3 +121,26 @@ function walk(s0; steps=10)
     end
     return stds
 end
+
+function show_stds(stds, qstds, steps)
+    xs = collect(0:steps)
+    plot(xs, stds, label="Classical Random Walk", linecolor=:blue, xlabel="Steps", ylabel="Standard Deviation", legend=:topleft, framestyle=:box, dpi=300)
+    plot!(xs, qstds, label="Quantum Random Walk simulation", linecolor=:red)
+    plot!([0, steps], [0, 0.54 * steps], label="Quantum Random Walk (≈ 0.54r)", linecolor=:black)
+    savefig("outputs/discrete_$(steps).png")
+end
+
+function run_Z_simulation(; steps=50)
+    qs0 = QState(1.0, 0, 0)
+    qstds = walk(qs0; steps=steps)
+    qstds
+
+    s0 = State(1.0, 0)
+    stds = walk(s0; steps=steps)
+    stds
+
+    show_stds(stds, qstds, steps)
+end
+
+run_Z_simulation(; steps=50)
+run_Z_simulation(; steps=10)
